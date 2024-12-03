f261abf221a3df3affb02773640a13633cf616b7
Emirhan ETLİ <emirhanetli2003@gmail.com>
2024-12-03 12:33:21 +0300

    test route

---
+++ app/index.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { useRouter } from 'expo-router';
+
+const Splash = () => {
+  const router = useRouter();
+
+  const logo = require("../assets/images/logo.png");
+
+  useEffect(() => {
+
+  }, [router]);
+
+  return (
+    <View style={styles.container}>
+      <Image source={logo}/>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  image: {
+    width: 150,
+    height: 150,
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#007BFF',
+  },
+});
+
+export default Splash;
---
+++ app/login/index.tsx
@@ -0,0 +1,80 @@
+
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from 'react-native';
+import { useRouter } from 'expo-router';
+import { Link, Stack } from 'expo-router';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    if (email === '' || password === '') {
+      Alert.alert('Hata', 'Lütfen tüm alanları doldurun.');
+    } else {
+      Alert.alert('Başarılı', 'Giriş başarılı!');
+    }
+  };
+
+  return (
+    <>
+    <Stack.Screen options={{ title: 'Login!' }} />
+    <View style={styles.container}>
+      <Text style={styles.title}>Giriş Yap</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="E-posta"
+        keyboardType="email-address"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Şifre"
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+      />
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Giriş Yap</Text>
+      </TouchableOpacity>
+    </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 24,
+  },
+  input: {
+    width: '100%',
+    padding: 12,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 8,
+    marginBottom: 16,
+  },
+  button: {
+    backgroundColor: '#007BFF',
+    padding: 12,
+    borderRadius: 8,
+    width: '100%',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+});
+
+export default Login;
