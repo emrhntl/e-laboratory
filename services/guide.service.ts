e9f559eb00d9bce7b22d7aec3f5964b47f0ec2f7
Emirhan ETLİ <emirhanetli2003@gmail.com>
2024-12-13 00:27:33 +0300

    added audit and guide services

---
+++ services/guide.service.ts
@@ -0,0 +1,19 @@
+import FirestoreManager from "@/constants/firestoreManager";
+import Analysis from "@/entity/analysis";
+import { Collections } from "@/enums/collections.enum";
+
+export default class AnalysisService extends FirestoreManager<Analysis> {
+
+    private static instance: AnalysisService;
+
+    constructor() {
+        super(Collections.GuideList);
+    }
+
+    public static getInstance(): AnalysisService {
+        if (!AnalysisService.instance) {
+            AnalysisService.instance = new AnalysisService();
+        }
+        return AnalysisService.instance;
+    }
+}
